4d431d3591f6afbcfbea03761103072c3ae88d88
Chris Atoki <christopher.atoki@yahoo.com>
2019-09-29 21:02:30 -0400

    Add extra donations pictures

--- src/components/Pages/PaymentPage/PaymentPage.js
+++ src/components/Pages/PaymentPage/PaymentPage.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { DONATE_CTA } from "../../../messages";
 import cashAppImg from "../../../assets/cash-app-qr-code.jpeg";
+import breastCancer from "../../../assets/breast-cancer.jpg";
+import cancerTreatment from "../../../assets/cancer-treatment.png";
+import humaneSociety from "../../../assets/humane-society.png";
+import stJude from "../../../assets/st-jude.jpg";
 
 export const PaymentPage = () => {
   return (
@@ -9,6 +13,12 @@ export const PaymentPage = () => {
       <div>
         <strong>Donate With Us on CashApp</strong>
         <img src={cashAppImg} alt="cash-app" style={{ margin: "0 auto", height: "250px", width: "250px" }} />
+        <div style={{ display: "flex", marginTop: "10px" }}>
+          <img src={breastCancer} alt="breast-cancer" style={{ margin: "0 auto", height: "100px", width: "150px" }} />
+          <img src={cancerTreatment} alt="cancer-treatment" style={{ margin: "0 auto", height: "100px", width: "150px" }} />
+          <img src={humaneSociety} alt="humane-society" style={{ margin: "0 auto", height: "100px", width: "150px" }} />
+          <img src={stJude} alt="st-jude" style={{ margin: "0 auto", height: "100px", width: "100px" }} />
+        </div>
       </div>
     </div>
   );
